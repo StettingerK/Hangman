9b53d51e0a63d0e7a3c773058aed939370ad1514
Kevin Stettinger <Kevin.Stettinger@rdf.nuernberg.de>
2025-12-12 08:15:06 +0100

    Daetein hinzugefügt

---
+++ darstellungsschicht.py
@@ -0,0 +1,11 @@
+def eingabe_Spieler() -> list:
+    pass
+
+def hangman_zeichnen(hangman_list:list[dict], i:int):
+    pass
+
+def wort_darstellen(ausgabe:str):
+    pass
+
+def buchstaben_raten() -> str:
+    pass
---
+++ main.py
@@ -0,0 +1,13 @@
+def buchstabe_pruefen(wort:str, buchstabe:str) -> bool:
+    pass
+
+def wort_pruefen(wort:str, pruef_wort:str) -> bool:
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
