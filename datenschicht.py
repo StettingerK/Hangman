ffe270116f1409040eab0f2bac85ddad7944dd67
Kevin Stettinger <Kevin.Stettinger@rdf.nuernberg.de>
2025-12-12 08:51:25 +0100

    funktion hangman_vorlage fertig

--- datenschicht.py
+++ datenschicht.py
@@ -1,7 +1,8 @@
 import random
+import json
 
 def wort_erzeugen() -> str:
-    with open("woerter.txt", "r") as file:
+    with open("woerter.txt", "r", encoding="utf-8") as file:
         data = file.readlines()
     woerter:list = []
     for line in data:
@@ -13,7 +14,13 @@ def wort_erzeugen() -> str:
     return woerter[r]
 
 def hangman_vorlage() -> list[dict]:
-    pass
+    with open("hangman.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+    # for line in data: # zum test
+    #     for a in line["graphic"]:
+    #         print(a)
+    return data
 
 if __name__ == "__main__":
     wort_erzeugen()
+    hangman_vorlage()
